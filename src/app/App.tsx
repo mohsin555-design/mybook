5c9ecd333de148e770269c7db8c9fa31d9e9f06f
Mohsin Ali Sayyed <designer.mohsinali@gmail.com>
2026-07-25 17:38:38 +0530

    pwa-home-screen-install

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
 
+import { PwaStatus } from '../components/common/PwaStatus'
 import { router } from './router'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return <>
+    <PwaStatus />
+    <RouterProvider router={router} />
+  </>
 }
